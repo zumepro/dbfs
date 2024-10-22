60692b63af09751006f94af9b6b89347890c44d7
Ondřej <mekina@duck.com>
2024-10-22 21:33:17 +0200

    add documentation

--- src/db_connector/connection_adapter.rs
+++ src/db_connector/connection_adapter.rs
@@ -4,7 +4,47 @@ use sqlx::{FromRow, MySql, MySqlPool, Pool};
 
 
 #[derive(Debug)]
+/// Database input type interface.
+/// Multiple datatypes can be automatically converted.
+///
+/// # Example usage
+/// ```rust
+/// db_conn.run_command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42.into()]));
+/// db_conn.run_command("INSERT INTO `test` (`char_value`) VALUES (?)", Some(&vec!["Hello, world!".into()]));
+/// db_conn.run_command("INSERT INTO `test` (`blob`) VALUES (?)", Some(&vec![vec![1_u8, 2_u8, 3_u8].into()]));
+/// ```
+///
+/// # Supported datatypes
+/// Automatic conversion can be done from the following datatypes: `i32`, `&str`, `String`, `Vec<u8>`
+pub enum DbInputType {
+    Integer(i32),
+    Char(String),
+    Blob(Vec<u8>),
+}
+impl Into<DbInputType> for i32 { fn into(self) -> DbInputType { DbInputType::Integer(self) } }
+impl Into<DbInputType> for String { fn into(self) -> DbInputType { DbInputType::Char(self) } }
+impl<'a> Into<DbInputType> for &'a str { fn into(self) -> DbInputType { DbInputType::Char(String::from(self)) } }
+impl Into<DbInputType> for Vec<u8> { fn into(self) -> DbInputType { DbInputType::Blob(self) } }
+
+
+#[derive(Debug)]
+/// SQL connection adapter.
 pub struct Adapter(Pool<MySql>);
+
+
+macro_rules! prepared_stmt_bind_args {
+    ($args: ident, $query: ident) => {
+        if let Some($args) = $args {
+            for arg in $args.iter() {
+                $query = match arg {
+                    DbInputType::Integer(val) => $query.bind(val),
+                    DbInputType::Char(val) => $query.bind(val),
+                    DbInputType::Blob(val) => $query.bind(val),
+                };
+            }
+        };
+    }
+}
 
 
 impl Adapter {
@@ -25,17 +65,24 @@ impl Adapter {
 
     /// Query is a database query which **expects data** as a return. Use `run_command` if
     /// no data is expected.
-    pub async fn run_query<I, T>(&mut self, query: &'static str, args: Option<&Vec<I>>) -> Result<Vec<T>, String>
+    ///
+    /// Will return `Ok(Vec<YourStruct>)` if the query was executed successfully and deserialized into
+    /// `YourStruct`s. Each row is deserialized into one `YourStruct`. The returned set contains all
+    /// collected rows.
+    ///
+    /// Will return `Err(String)` if there was an error while processing the query. The inner
+    /// value is either returned directly from the server or contains connection fail info.
+    ///
+    /// # Example usage
+    /// ```rust
+    /// let rows: Vec<MyStruct> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![1.into()])).await.unwrap();
+    /// ```
+    pub async fn run_query<T>(&mut self, query: &'static str, args: Option<&Vec<DbInputType>>) -> Result<Vec<T>, String>
     where 
-        I: for<'r> sqlx::Encode<'r, sqlx::MySql> + sqlx::Type<sqlx::MySql>,
         T: for<'r> FromRow<'r, sqlx::mysql::MySqlRow>
     {
         let mut query = sqlx::query(query);
-        if let Some(args) = args {
-            for arg in args.iter() {
-                query = query.bind(arg);
-            }
-        };
+        prepared_stmt_bind_args!(args, query);
         let mut query_result = query.fetch(&self.0);
         let mut result = Vec::new();
         while let Some(row) = query_result.try_next().await.map_err(|err| format!("{}", err))? {
@@ -46,16 +93,19 @@ impl Adapter {
 
 
     /// Command is a query **without expected response data**. Use `run_query` if data is expected.
-    pub async fn run_command<T>(&mut self, command: &'static str, args: Option<&Vec<T>>) -> Result<(), String>
-    where 
-        T: for<'r> sqlx::Encode<'r, sqlx::MySql> + sqlx::Type<sqlx::MySql>
-    {
+    ///
+    /// Will return `Ok(())` if the command was executed successfully.
+    ///
+    /// Will return `Err(String)` if there was an error while processing the command. The inner
+    /// value is either returned directly from the server or contains connection fail info.
+    ///
+    /// # Example usage
+    /// ```rust
+    /// adpt.run_command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42.into()])).await.unwrap();
+    /// ```
+    pub async fn run_command(&mut self, command: &'static str, args: Option<&Vec<DbInputType>>) -> Result<(), String> {
         let mut query = sqlx::query(command);
-        if let Some(args) = args {
-            for arg in args.iter() {
-                query = query.bind(arg);
-            }
-        };
+        prepared_stmt_bind_args!(args, query);
         query.execute(&self.0).await.map_err(|err| format!("{}", err))?;
         Ok(())
     }
@@ -71,7 +121,7 @@ mod test {
     #[tokio::test]
     async fn test_run_command() {
         let mut adpt = Adapter::default().await.unwrap();
-        let result = adpt.run_command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42_u32])).await;
+        let result = adpt.run_command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42.into()])).await;
         assert_eq!(result, Ok(()));
     }
 
@@ -85,21 +135,34 @@ mod test {
     #[tokio::test]
     async fn test_run_select_01() {
         let mut adpt = Adapter::default().await.unwrap();
-        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![1])).await.unwrap();
+        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![1.into()])).await.unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 1, test_name: "aaa".to_string() }]);
     }
 
     #[tokio::test]
     async fn test_run_select_02() {
         let mut adpt = Adapter::default().await.unwrap();
-        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![2])).await.unwrap();
+        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![2.into()])).await.unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 2, test_name: "bbb".to_string() }]);
     }
 
     #[tokio::test]
     async fn test_run_select_03() {
         let mut adpt = Adapter::default().await.unwrap();
-        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3])).await.unwrap();
+        let rows: Vec<TestPrepared> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3.into()])).await.unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 3, test_name: "ccc".to_string() }]);
+    }
+
+    #[derive(FromRow, Debug, PartialEq)]
+    struct TestPrepared02 {
+        id: i32,
+        test_name: Vec<u8>,
+    }
+
+    #[tokio::test]
+    async fn test_run_select_04() {
+        let mut adpt = Adapter::default().await.unwrap();
+        let rows: Vec<TestPrepared02> = adpt.run_query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3.into()])).await.unwrap();
+        assert_eq!(rows, vec![TestPrepared02 { id: 3, test_name: vec!['c' as u8, 'c' as u8, 'c' as u8] }]);
     }
 }
--- src/db_connector/mod.rs
+++ src/db_connector/mod.rs
@@ -1,13 +1,40 @@
 pub use sqlx::FromRow;
-use tokio::runtime::Runtime;
 use tokio::runtime::Builder;
 
 
 use connection_adapter::Adapter;
+pub use connection_adapter::DbInputType;
 mod connection_adapter;
 
 
 #[derive(Debug, PartialEq)]
+/// Generic error that can be returned when interacting with a database.
+///
+/// # Subtypes
+///
+/// ```rust
+/// RuntimeStartFail
+/// ```
+/// This error indicates that an async wrapper could not be started to transform async network IO
+/// to a blocking context.
+///
+///
+/// ```rust
+/// AdapterError(String)
+/// ```
+/// The inner SQL adapter encountered an error while interacting with the database.
+/// This is usually some SQL error (such as SQL command/query syntax error).
+///
+/// # Conversion
+///
+/// This enum can be easily converted into a `String` as it implements `std::fmt::Display` and
+/// `Into<String>`.
+///
+/// However explicit mapping is recommended.
+/// ```rust
+/// value_with_db_connector_error.map_err(|err| err.into::<String>());
+/// ```
+///
 pub enum DbConnectorError {
     RuntimeStartFail,
     AdapterError(String),
@@ -16,13 +43,27 @@ pub enum DbConnectorError {
 impl std::fmt::Display for DbConnectorError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "db_connector_error: {}", match self {
-            Self::RuntimeStartFail => "could not start async runner".to_string(),
+            Self::RuntimeStartFail => "db_connector_error: could not start async runner".to_string(),
             Self::AdapterError(val) => format!("sql_adapter: {}", val),
         })
     }
 }
+impl Into<String> for DbConnectorError { fn into(self) -> String { format!("{}", self) } }
 
 
+/// Interface for database communication
+///
+/// # Creating a `DbConnector`
+/// The recommended method is `DbConnector::default()` - this method will use values from
+/// `crate::settings` to find the host, port, user, password and database.
+///
+/// # Command
+/// A command is an SQL query that expects no results (just success or fail). Command has a separate method to save
+/// resources.
+/// ```rust
+/// db_connector.query("INSERT INTO `table` (`id`, `value`) VALUES (?, ?)", vec![])
+/// ```
+///
 #[derive(Debug)]
 pub struct DbConnector {
     adapter: connection_adapter::Adapter,
@@ -51,19 +92,38 @@ impl DbConnector {
         }));
     }
 
-    pub fn command<T>(&mut self, command: &'static str, args: Option<&Vec<T>>) -> Result<(), DbConnectorError>
-    where 
-        T: for<'r> sqlx::Encode<'r, sqlx::MySql> + sqlx::Type<sqlx::MySql>
-    {
+    /// A command is an SQL query with **no expected response data**.
+    /// 
+    /// `Ok(())` is returned on success.
+    ///
+    /// `Err(DbConnectorError)` is returned on fail.
+    ///
+    /// # Example usage
+    /// ```rust
+    /// conn.command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42.into()])).unwrap();
+    /// ```
+    pub fn command(&mut self, command: &'static str, args: Option<&Vec<DbInputType>>) -> Result<(), DbConnectorError> {
         blockingify!({
             self.adapter.run_command(command, args).await.map_err(|err| DbConnectorError::AdapterError(err))?;
             Ok(())
         });
     }
 
-    pub fn query<I, T>(&mut self, query: &'static str, args: Option<&Vec<I>>) -> Result<Vec<T>, DbConnectorError>
+    /// A query is an SQL query with **expected response data**.
+    /// 
+    /// `Ok(Vec<YourStruct>)` is returned on success.
+    ///
+    /// `Err(DbConnectorError)` is returned on fail.
+    ///
+    /// This function will try to deserialize the response into `YourStruct`.
+    /// `FromRow` must be derived (or implemented) for `YourStruct` to deserialize successfully.
+    ///
+    /// # Example usage
+    /// ```rust
+    /// let rows: Vec<MyStruct> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3.into()])).unwrap();
+    /// ```
+    pub fn query<T>(&mut self, query: &'static str, args: Option<&Vec<DbInputType>>) -> Result<Vec<T>, DbConnectorError>
     where
-        I: for<'r> sqlx::Encode<'r, sqlx::MySql> + sqlx::Type<sqlx::MySql>,
         T: for<'r> FromRow<'r, sqlx::mysql::MySqlRow>
     {
         blockingify!({
@@ -82,7 +142,7 @@ mod test {
     #[test]
     fn test_run_command() {
         let mut conn = DbConnector::default().unwrap();
-        let result = conn.command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42_u32]));
+        let result = conn.command("INSERT INTO `test` (`id`) VALUES (?)", Some(&vec![42.into()]));
         assert_eq!(result, Ok(()));
     }
 
@@ -96,21 +156,21 @@ mod test {
     #[test]
     fn test_run_select_01() {
         let mut conn = DbConnector::default().unwrap();
-        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![1])).unwrap();
+        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![1.into()])).unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 1, test_name: "aaa".to_string() }]);
     }
 
     #[test]
     fn test_run_select_02() {
         let mut conn = DbConnector::default().unwrap();
-        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![2])).unwrap();
+        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![2.into()])).unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 2, test_name: "bbb".to_string() }]);
     }
 
     #[test]
     fn test_run_select_03() {
         let mut conn = DbConnector::default().unwrap();
-        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3])).unwrap();
+        let rows: Vec<TestPrepared> = conn.query("SELECT * FROM `test_prepared` WHERE `id` = ?", Some(&vec![3.into()])).unwrap();
         assert_eq!(rows, vec![TestPrepared { id: 3, test_name: "ccc".to_string() }]);
     }
 }
