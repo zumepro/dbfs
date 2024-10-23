2eb6bcd4c3e2d57b5b6c79cf9b96b912b47c7520
Michal Procházka <michal@prochazkaml.eu>
2024-10-23 15:27:54 +0200

    Tabs are superior

--- src/fuse_driver/mod.rs
+++ src/fuse_driver/mod.rs
@@ -19,46 +19,46 @@ impl DbfsDriver {
 const TTL: Duration = Duration::from_secs(1);
 
 impl fuser::Filesystem for DbfsDriver {
-    fn lookup(&mut self, _req: &fuser::Request, parent_inode: u64, name: &OsStr, reply: fuser::ReplyEntry) {
+	fn lookup(&mut self, _req: &fuser::Request, parent_inode: u64, name: &OsStr, reply: fuser::ReplyEntry) {
 		match self.tl.lookup(name, parent_inode) {
 			Ok(attr) => reply.entry(&TTL, &attr, 0),
 			Err(_) => reply.error(ENOENT)
 		}
-    }
+	}
 
-    fn getattr(&mut self, _req: &fuser::Request, inode: u64, reply: fuser::ReplyAttr) {
-        match self.tl.getattr(inode) {
+	fn getattr(&mut self, _req: &fuser::Request, inode: u64, reply: fuser::ReplyAttr) {
+		match self.tl.getattr(inode) {
 			Ok(attr) => reply.attr(&TTL, &attr),
 			Err(_) => reply.error(ENOENT)
 		}
-    }
+	}
 
-    fn read(
-        &mut self,
-        _req: &fuser::Request,
-        inode: u64,
-        _fh: u64,
-        offset: i64,
-        size: u32,
-        _flags: i32,
-        _lock: Option<u64>,
-        reply: fuser::ReplyData,
-    ) {
+	fn read(
+		&mut self,
+		_req: &fuser::Request,
+		inode: u64,
+		_fh: u64,
+		offset: i64,
+		size: u32,
+		_flags: i32,
+		_lock: Option<u64>,
+		reply: fuser::ReplyData,
+	) {
 		let mut buf = vec![0u8; size as usize];
 		match self.tl.read(inode, offset as u64, &mut buf) {
 			Ok(()) => reply.data(&buf),
 			Err(_) => reply.error(ENOENT)
 		}
-    }
+	}
 
-    fn readdir(
-        &mut self,
-        _req: &fuser::Request,
-        inode: u64,
-        _fh: u64,
-        offset: i64,
-        mut reply: fuser::ReplyDirectory,
-    ) {
+	fn readdir(
+		&mut self,
+		_req: &fuser::Request,
+		inode: u64,
+		_fh: u64,
+		offset: i64,
+		mut reply: fuser::ReplyDirectory,
+	) {
 		let mut i = offset;
 		
 		loop {
@@ -80,13 +80,13 @@ impl fuser::Filesystem for DbfsDriver {
 		}
 
 		reply.ok();
-    }
+	}
 }
 
 pub fn run_forever(tl: TranslationLayer, mountpoint: &str) -> ! {
-    let options = vec![fuser::MountOption::RO, fuser::MountOption::FSName("hello".to_string()), fuser::MountOption::AutoUnmount, fuser::MountOption::AllowRoot];
+	let options = vec![fuser::MountOption::RO, fuser::MountOption::FSName("hello".to_string()), fuser::MountOption::AutoUnmount, fuser::MountOption::AllowRoot];
 	let driver = DbfsDriver::new(tl);
-    fuser::mount2(driver, mountpoint, &options).unwrap();
+	fuser::mount2(driver, mountpoint, &options).unwrap();
 	panic!("FUSE driver crashed");
 }
 
