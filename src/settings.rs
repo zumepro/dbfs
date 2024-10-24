53376ba0463f5b6f7adb466e6d548e11ac927f95
Ondřej Mekina <mekina@duck.com>
2024-10-25 00:16:23 +0200

    hide settings from rustdoc

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,8 @@
+#[doc(hidden)]
 pub const SQL_HOST: &'static str = "[::1]:3306";
+#[doc(hidden)]
 pub const SQL_USER: &'static str = "dbfs";
+#[doc(hidden)]
 pub const SQL_PASSWD: &'static str = "dbfs";
+#[doc(hidden)]
 pub const SQL_DB: &'static str = "dbfs";
