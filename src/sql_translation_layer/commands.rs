1a4329987a4496d44d85d51493fa630024d98e7a
Michal Procházka <michal@prochazkaml.eu>
2024-10-30 11:11:48 +0100

    Optimized file size SQL

--- src/sql_translation_layer/commands.rs
+++ src/sql_translation_layer/commands.rs
@@ -7,12 +7,10 @@
 /// # Columns
 /// - `bytes`
 /// - `blocks`
-pub const SQL_GET_FILE_SIZE: &'static str = r#"WITH `ino` AS (SELECT ? AS `ino`), `file_tmp` (`blocks`) AS (
-    SELECT COUNT(*) FROM `block` WHERE `inode_id` = (SELECT `ino` FROM `ino`)
-) SELECT
-    `blocks` * 4096 - (SELECT 4096 - OCTET_LENGTH(`data`) FROM `block` WHERE `inode_id` = (SELECT `ino` FROM `ino`) ORDER BY `block_id` DESC LIMIT 1) AS bytes,
-    `blocks` AS blocks
-FROM `file_tmp`"#;
+pub const SQL_GET_FILE_SIZE: &'static str = r#"SELECT
+	(`block_id` - 1) * 4096 + OCTET_LENGTH(`data`) as `bytes`,
+	`block_id` as `blocks`
+FROM `block` WHERE `inode_id` = ? ORDER BY `block_id` DESC LIMIT 1"#;
 
 
 /// # Binds
