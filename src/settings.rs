50814321ff938410d0e0ef4404360340b559c5eb
Ondřej Mekina <mekina@duck.com>
2024-10-30 15:49:00 +0100

    add block size changing

--- src/settings.rs
+++ src/settings.rs
@@ -8,6 +8,14 @@ pub const SQL_PASSWD: &'static str = "dbfs";
 pub const SQL_DB: &'static str = "dbfs";
 
 
+macro_rules! block_size{
+    () => {
+        4096
+    }
+}
+
+
+// Please don't touch this
 #[cfg(feature = "integration_testing")]
 pub const FILE_BLOCK_SIZE: u64 = 4096;
 #[cfg(feature = "integration_testing")]
@@ -15,8 +23,8 @@ pub const FILE_BLOCK_SIZE_32: u32 = 4096;
 #[cfg(feature = "integration_testing")]
 pub const FILE_BLOCK_SIZE_USIZE: usize = 4096;
 #[cfg(not(feature = "integration_testing"))]
-pub const FILE_BLOCK_SIZE: u64 = 4096;
+pub const FILE_BLOCK_SIZE: u64 = block_size!();
 #[cfg(not(feature = "integration_testing"))]
-pub const FILE_BLOCK_SIZE_32: u32 = 4096;
+pub const FILE_BLOCK_SIZE_32: u32 = block_size!();
 #[cfg(not(feature = "integration_testing"))]
-pub const FILE_BLOCK_SIZE_USIZE: usize = 4096;
+pub const FILE_BLOCK_SIZE_USIZE: usize = block_size!();
